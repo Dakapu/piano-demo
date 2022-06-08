ba3c2b8c4f79693a275f8b0829d8a00c71d11c7e
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-08 17:38:45 +0200

    implement config for babel (fontawesome req)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid, regular } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 function App() {
-  return div.container;
+  return (
+    <div class="container">
+      <h1>This is a Test for Fontawesome</h1>
+      <ul>
+        <li>
+          <FontAwesomeIcon icon={solid("paw")} />
+          Pet
+        </li>
+        <li>
+          <FontAwesomeIcon icon={solid("car")} />
+          Car
+        </li>
+        <li>
+          <FontAwesomeIcon icon={regular("map")} />
+          House
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
