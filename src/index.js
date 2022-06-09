dcd165fa34a850ecdb3b9f57c5505e3a6524987d
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-09 10:30:32 +0200

    register ServiceWorker for transforming App to PWA

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
+import "./scss/index.scss";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
