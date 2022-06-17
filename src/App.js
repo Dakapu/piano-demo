c9c05fe4ded98be2dac07d64e9c92ad94dbd2482
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-17 18:33:33 +0200

    add tailwind navbar

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import "./scss/App.scss";
 function App() {
   return (
     <div className="container">
-      <h1>This is a Test for Fontawesome</h1>
       <NavBar />
+      <div className="h-screen ml-48 container ">
+        <h1 className="font-medium text-5xl text-white">
+          This is a Test for Fontawesome
+        </h1>
+      </div>
     </div>
   );
 }
