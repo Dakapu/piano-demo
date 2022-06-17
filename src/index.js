c58a27d159fe7b4faec9977cf38b0a91ca516c3e
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-17 17:56:12 +0200

    set background color for entire page

--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <div className="w-full h-screen bg-gradient-to-br from-slate-900 to-teal-500 ">
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </div>
 );
 
 serviceWorkerRegistration.register();
