769f66e07d04614d63a97e172ca84a4a81ef314b
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-19 19:19:37 +0200

    add content card

--- src/common/instrument.jsx
+++ src/common/instrument.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import "../scss/instrument.scss";
 
 const Instrument = () => {
   return (
     <div className="container  mx-auto text-center">
-      <h1 className="uppercase pt-10 text-white">
-        This is a Test for Fontawesome
-      </h1>
+      <div className="card text-center col-10 mx-auto">
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
