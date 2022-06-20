ccd70c5c663eb1604ad6791bb91fc7940801f336
Dakapu <24271657+Dakapu@users.noreply.github.com>
2022-06-20 13:44:23 +0200

    implement flute fret

--- src/components/flute.jsx
+++ src/components/flute.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import fluteBoard from "../img/flute-board.svg";
 
 const Flute = () => {
-  return null;
+  return <img className="p-5 w-25" src={fluteBoard} alt="flute board" />;
 };
 
 export default Flute;
